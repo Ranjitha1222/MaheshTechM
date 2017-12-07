2f2ccaa7280a925cc2f68aaeaf7d83c1c2335046
Ranjitha1222 <34330662+Ranjitha1222@users.noreply.github.com>
2017-12-07 15:51:23 +0530

    third commit

--- Sampletest/src/com/test/practice.java
+++ Sampletest/src/com/test/practice.java
@@ -5,7 +5,7 @@ public class practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("test");
+		System.out.println("test87346872314");
 		System.out.println("Thisd sis the sammple programme");
 	}
 
